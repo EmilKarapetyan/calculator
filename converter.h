8ea7db8a2b43196d11dfbcbe2eb6476fd5304022
Emil Karapetyan <ekarape@internal.synopsys.com>
2019-03-14 18:16:32 +0400

    whole program

---
+++ converter.h
@@ -0,0 +1,26 @@
+#include<stack>
+#include<string>
+#include<sstream>
+
+#ifndef CONVERTER_H
+#define CONVERTER_H
+
+class Convert
+{
+
+public:
+	Convert() = default;
+	static double eval_postfix(const std::string&);
+	static double perform_operation(const char, double, double);
+	static bool areParanthesisBalanced(const std::string &);
+	static std::string to_postfix(const std::string&);
+
+	static bool is_par(const char);
+	static bool is_operand(const char);
+	static bool is_operator(const char);
+	static int priority(const char);
+
+};
+
+#endif // !CONVERTER_H
+
---
+++ main.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include <string>
+#include"converter.h"
+#include"assembler.h"
+
+int main()
+{
+	//std::string input = "(-2*(5+9)-14)";
+
+	//std::string output = Convert::to_postfix(input);
+	////std::cout << std::endl << Convert::eval_postfix(output) << std::endl << std::endl;
+
+	//Assembler::to_assembler(input);
+
+
+	std::string input = " ";
+
+	while (input.size())
+	{
+		std::cout << "Input your Expression :\n\n";
+		getline(std::cin, input);
+
+		std::string output = Convert::to_postfix(input);
+		std::cout << std::endl << Convert::eval_postfix(output) << std::endl << std::endl;
+
+	}
+
+	system("pause");
+	return 0;
+}
