54c889eea9ee43216482a8926f2b42d589dfd81b
EmilKarapetyan <31102182+EmilKarapetyan@users.noreply.github.com>
2019-03-15 12:21:38 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,6 @@
 
 int main()
 {
-	//std::string input = "(-2*(5+9)-14)";
-
-	//std::string output = Convert::to_postfix(input);
-	////std::cout << std::endl << Convert::eval_postfix(output) << std::endl << std::endl;
-
-	//Assembler::to_assembler(input);
-
-
 	std::string input = " ";
 
 	while (input.size())
